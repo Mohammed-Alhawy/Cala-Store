37a88be16d7bc8fa8e2a10f8f572cce3481b8a9a
Mohammed <alhawymohammed2@gmail.com>
2025-04-14 23:45:11 +0200

    editing

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ menuIcon.addEventListener("click" , ()=>{
 
 close.addEventListener("click" , ()=>{
   menu.classList.remove("Active-menu")
-  console.log("ll");
 })
 
 
@@ -27,3 +26,47 @@ smalImgCol.forEach( item =>{
   })
 })
 
+
+
+
+
+
+/********* big-cart-Active *********/
+
+
+const openCart=document.querySelectorAll(".open")
+const bigCart=document.querySelector(".big-cart")
+
+openCart.forEach(item =>{
+  item.addEventListener("click" , ()=>{
+    bigCart.classList.toggle("big-cart-Active")
+  })
+})
+
+
+// console.log(btn.parentElement.getElementsByTagName("img")[0].getAttribute("src"));
+/********* add products to the cart *********/
+
+const cartBtns=document.querySelectorAll(".cart")
+const cartItem=document.querySelector(".big-cart table tbody")
+console.log(cartItem)
+
+cartBtns.forEach(btn =>{
+  btn.addEventListener("click" , ()=>{
+
+    cartItem.innerHTML+=`
+      <tr>
+            <td><a href="#"><i class="ri-close-circle-line"></i></a></td>
+            <td><img src="${btn.parentElement.getElementsByTagName("img")[0].getAttribute("src")}" alt=""></td>
+            <td>Cartoon Astronaut T-Shirts</td>
+            <td>${ btn.parentElement.getElementsByClassName("des")[0].getElementsByTagName("h4")[0].innerText }</td>
+            <td><input type="number" value="1"></td>
+            <td></td>
+          </tr>
+    `
+
+
+
+
+  })
+})
